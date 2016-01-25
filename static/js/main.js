84c1e76174b34211ec819184c71b195d54a8c876
Shipeng Xu <billhsu.x@gmail.com>
2016-01-25 21:59:43 +0800

    update website

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $('.btn-mobile-menu').click(function () {
+    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+  })
+})
