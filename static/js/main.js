a5407fb475f8929d904c6d50e6406d82e6bee74a
Shipeng Xu <billhsu.x@gmail.com>
2016-01-29 18:01:47 +0800

    new version

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,29 @@
-$(document).ready(function () {
-  $('.btn-mobile-menu').click(function () {
-    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
-})
+(function() {
+  $(function() {
+    var centerElements;
+    centerElements = function() {
+      var $el, el, elHeight, i, len, ref, results, windowHeight;
+      windowHeight = window.innerHeight;
+      ref = $('.js-vertical-center');
+      results = [];
+      for (i = 0, len = ref.length; i < len; i++) {
+        el = ref[i];
+        $el = $(el);
+        elHeight = $el.innerHeight();
+        if (elHeight < windowHeight) {
+          results.push($el.css({
+            top: ((windowHeight - elHeight) / 2) + "px"
+          }));
+        } else {
+          results.push($el.css({
+            top: 0
+          }));
+        }
+      }
+      return results;
+    };
+    centerElements();
+    return $(window).on('resize', centerElements);
+  });
+
+}).call(this);
